5aa67a2a253f107c36ce5ac877830a41ccc65ba0
PiemonteF <fabio.lopes@sou.inteli.edu.br>
2022-11-29 15:00:37 -0300

    UPDATE LOGIN
    Login atualizado - utiliza apenas um endpoint

--- src/routes/login.js
+++ src/routes/login.js
@@ -14,7 +14,7 @@ router.post("/autenticacao", (req, res) => {
     let email = req.body["email"];
     let senha = req.body["senha"];
 
-    const sql = `
+    let sql = `
         SELECT *
         FROM empreiteira
         WHERE 
@@ -28,56 +28,34 @@ router.post("/autenticacao", (req, res) => {
             console.error(err.message);
             res.send("Erro: bobao " + err.message);
             return;
-        }
-        //res.json(rows);
-        //se não possuir conta, volta para a página de login
-        console.log(rows);
-        if (rows === undefined) {
-            res.render("login/login");
-        } else{
+        }else if(rows !== undefined){
             res.render("MRV_admin/usuario-logado-admin");
         };
-        //encaminha para a página feed do admin da mrv
-
-        
     });
-});
 
-router.all("/admin", (req, res) => {
-    res.render("login/login_admin");
-});
-//autenticacão do login
-router.post("/autenticacao_admin", (req, res) => {
-    let email = req.body["email"];
-    let senha = req.body["senha"];
+    console.log("oi");
 
-    const sql = `
-        SELECT *
-        FROM administrador_mrv
-        WHERE 
-            senha=? AND 
-            email=?`;
-
-    console.log(sql);
-
+    sql = `
+    SELECT *
+    FROM administrador_mrv
+    WHERE
+        senha = ? and
+        email = ?`;
+        
     db.get(sql, [senha, email], (err, rows) =>{
         if(err) {
             console.error(err.message);
             res.send("Erro: bobao " + err.message);
             return;
-        }
-        //res.json(rows);
-        //se não possuir conta, volta para a página de login
-        console.log(rows);
-        if (rows === undefined) {
-            res.render("login/login");
-        } else{
+        }else if(rows !== undefined){
             res.render("MRV_admin/usuario-logado-admin");
+        }else{
+            res.render("login/login");
         };
-        //encaminha para a página feed do admin da mrv
-
-        
     });
+
+
 });
+
 //exporta a rota para poder ser requisitada no app.js
 module.exports = router;
